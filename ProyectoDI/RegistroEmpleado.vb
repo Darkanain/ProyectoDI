47477ac016f745b51304b465a428fa5cd524f289
Darkanain <darkanain@gmail.com>
2017-03-21 09:47:00 +0100

    Mejorada v3

--- ProyectoDI/RegistroEmpleado.vb
+++ ProyectoDI/RegistroEmpleado.vb
@@ -1,7 +1,7 @@
 ﻿Public Class RegistroEmpleado
     Dim Resultado As MsgBoxResult
     Private Sub fPrincipal_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-
+        'Comprobacion por si desea salir de la aplicacion
         Resultado = CType(MessageBox.Show("¿Desea salir de la apliacion?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question), MsgBoxResult)
 
         If Resultado = MsgBoxResult.No Then
@@ -12,6 +12,7 @@
     End Sub
 
     Private Sub btnConf_Click(sender As Object, e As EventArgs) Handles btnConf.Click
+        'Control cuando se confirme el registro
         MessageBox.Show("Reserva confirmada", "Confirmación", MessageBoxButtons.OK)
 
         RemoveHandler MyBase.FormClosing, AddressOf fPrincipal_FormClosing
@@ -19,6 +20,7 @@
     End Sub
 
     Private Sub btnCanc_Click(sender As Object, e As EventArgs) Handles btnCanc.Click
+        'Control de salida del registro
         Resultado = CType(MessageBox.Show("¿Desea cancelar el registro?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question), MsgBoxResult)
 
         If Resultado = MsgBoxResult.No Then
@@ -30,6 +32,7 @@
     End Sub
 
     Private Sub Reg_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        'Cuando pulsamos F1 que nos mande a una pagina de ayuda
         If e.KeyCode = Keys.F1 Then
             System.Diagnostics.Process.Start("https://www.gambas-es.org/")
         End If
--- ProyectoDI/fPrincipal.vb
+++ ProyectoDI/fPrincipal.vb
@@ -1,6 +1,7 @@
 ﻿Public Class fPrincipal
     Dim resource As String
-    Private Sub idioma_Click(sender As Object, e As EventArgs) Handles selectEsp.Click, selectEng.Click
+    Public Sub idioma_Click(sender As Object, e As EventArgs) Handles selectEsp.Click, selectEng.Click
+        'Hacemos la comprobacion del idioma
         If sender.Equals(selectEsp) Then
             If selectEng.Checked = True Then
                 selectEng.Checked = False
@@ -26,6 +27,7 @@
 
     Dim Resultado As MsgBoxResult
     Private Sub fPrincipal_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        'Segun el idioma saca el mensaje en un idioma especifico
         If resource = "en" Then
             Resultado = CType(MessageBox.Show("Do you want to exit the application?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question), MsgBoxResult)
         Else
@@ -42,6 +44,7 @@
     Private Sub btnRNormal_Click(sender As Object, e As EventArgs) Handles btnRNormal.Click
         Dim frm As New RegistroEmpleado()
 
+        'Pasamos el logo del from y la imagen principal
         frm.ControlRegistro.setpbLogo(My.Resources.rlogo2)
 
         frm.Icon = My.Resources.logo2
@@ -52,6 +55,7 @@
     Private Sub btnRLol_Click(sender As Object, e As EventArgs) Handles btnRLol.Click
         Dim frm As New RegistroEmpleado()
 
+        'Pasamos el logo del from y la imagen principal
         frm.ControlRegistro.setpbLogo(My.Resources.rlogo1)
 
         frm.Icon = My.Resources.logo1
@@ -60,10 +64,12 @@
     End Sub
 
     Private Sub ayudaOnline_Click(sender As Object, e As EventArgs) Handles ayudaOnline.Click
+        'Cuando pulsamos el boton de ayuda que nos mande a esta pagina
         System.Diagnostics.Process.Start("https://www.gambas-es.org/")
     End Sub
 
     Private Sub Reserv_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        'Cuando pulsamos F1 que nos mande a una pagina de ayuda
         If e.KeyCode = Keys.F1 Then
             ayudaOnline_Click(Nothing, Nothing)
         End If
